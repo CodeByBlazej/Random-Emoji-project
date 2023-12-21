2f67f603aa51ea5c791ea54c9a953ffac6cb3d47
CodeByBlazej <codebyblazej@gmail.com>
2023-12-21 18:57:17 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const btnEl = document.getElementById('btn');
 const emojiNameEl = document.getElementById('emoji-name');
 
+const emoji = [];
 
+async function getEmoji() {
+  let response = await fetch('https://emoji-api.com/emojis?access_key=360f74f747e289bdf7b0163e930e8eb89b0335af')
 
+  data = await response.json();
 
+  for (let i=0; i<1500; i++) {
+    emoji.push({
+      emojiName: data[i].character,
+      emojiCode: data[i].unicodeName,
+    })
+  }
+}
+
+getEmoji();
 
 
 btnEl.addEventListener('click', () => {
-  console.log('clicked');
+  const randomNum = Math.floor(Math.random() * emoji.length);
+  btnEl.innerText = emoji[randomNum].emojiName;
+  emojiNameEl.innerText = emoji[randomNum].emojiCode;
 });
